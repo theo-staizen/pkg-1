8875473ec29d448b1c39ccbfae4cad87189c477f
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2023-11-22 12:49:19 +0200

    update log logic

--- colors.mjs
+++ colors.mjs
@@ -1,23 +1,12 @@
 import supportsColor from 'supports-color';
 import fs from 'fs';
 
-let output = '';
+let output = 'Do we support colors?\n';
+const addMessage = (message) => output += message + '\n';
 
-const addMessage = (message) => {
-    output += message + '\n';
-};
-
-if (supportsColor.stdout) {
-    addMessage('Terminal stdout supports color');
-}
-
-if (supportsColor.stdout.has256) {
-    addMessage('Terminal stdout supports 256 colors');
-}
-
-if (supportsColor.stderr.has16m) {
-    addMessage('Terminal stderr supports 16 million colors (truecolor)');
-}
+addMessage('Terminal stdout supports color: ' + !!supportsColor.stdout);
+addMessage('Terminal stdout supports 256 colors: ' + !!supportsColor.stdout.has256);
+addMessage('Terminal stderr supports 16 million colors (truecolor): ' + !!supportsColor.stderr.has16m);
 
 fs.writeFileSync('output.log', output, 'utf8');
 
