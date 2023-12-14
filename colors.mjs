0634bd042e10ff88c98ca9c4c191ee703bf984e5
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2023-12-14 18:26:37 +0200

    simplify log

--- colors.mjs
+++ colors.mjs
@@ -3,13 +3,12 @@ import fs from 'fs';
 
 const pkg = fs.readFileSync('./package.json', 'utf-8');
 
-console.log('Version:', JSON.parse(pkg).version)
-
-let output = 'Do we support colors?\n';
-const addMessage = (message) => output += message + '\n';
-
-addMessage('Terminal stdout supports color: ' + !!supportsColor.stdout);
-addMessage('Terminal stdout supports 256 colors: ' + !!supportsColor.stdout.has256);
-addMessage('Terminal stderr supports 16 million colors (truecolor): ' + !!supportsColor.stderr.has16m);
+let output = `
+Version: ${JSON.parse(pkg).version}
+Do we support colors?
+Terminal stdout supports color: ${!!supportsColor.stdout}
+Terminal stdout supports 256 colors: ${!!supportsColor.stdout.has256}
+Terminal stderr supports 16 million colors (truecolor): ${!!supportsColor.stderr.has16m}
+`;
 
 fs.writeFileSync('output.log', output, 'utf8');
