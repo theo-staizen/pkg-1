a827d70ea7d74d7083bdf42aad8e55f93f638507
Theodoros Antoniou <theodoros.antoniou@staizen.com>
2023-12-14 18:11:37 +0200

    log version

--- colors.mjs
+++ colors.mjs
@@ -1,6 +1,10 @@
 import supportsColor from 'supports-color';
 import fs from 'fs';
 
+const pkg = fs.readFileSync('./package.json', 'utf-8');
+
+console.log('Version:', JSON.parse(pkg).version)
+
 let output = 'Do we support colors?\n';
 const addMessage = (message) => output += message + '\n';
 
@@ -9,4 +13,3 @@ addMessage('Terminal stdout supports 256 colors: ' + !!supportsColor.stdout.has2
 addMessage('Terminal stderr supports 16 million colors (truecolor): ' + !!supportsColor.stderr.has16m);
 
 fs.writeFileSync('output.log', output, 'utf8');
-
